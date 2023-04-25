d2047d07f4f224db49f9656c133b14e046e0fc40
Rasmus Henriksen Klokker <B059064@PROD.SITAD.DK>
2023-04-25 23:02:26 +0200

    Added slides and source material for weeks 1-3

---
+++ docs/sides_source/week 2/resi_plot/run_resi_plot.R
@@ -0,0 +1,4 @@
+
+library(ggplot2)
+
+shiny::runApp("C:/Users/B059064/Desktop/PHD/papers/TTC dif in dif/PHD_dissertation/resi_plot/app.r", launch.browser = TRUE)
---
+++ docs/sides_source/week 2/resi_plot/run_resi_plot.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\R\R-4.2.0\bin\x64\R.exe" CMD BATCH "C:\Users\B059064\Desktop\PHD\papers\TTC dif in dif\PHD_dissertation\resi_plot\run_resi_plot.R"
